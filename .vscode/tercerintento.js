5efb63afd50021eb9d5fad6d84576ca4ef19b948
elProfeAle <37826866@ivt.edu.ar>
2025-09-23 09:48:14 -0300

    23/09/2025

---
+++ .vscode/tercerintento.js
@@ -0,0 +1,12 @@
+let fotos = document.querySelector('#fotos')
+let titulo = document.querySelector(`#titulo`)
+
+console.log('hola')
+fetch('https://api.nasa.gov/planetary/apod?api_key=Xecgl1F8ZtSq9Z0e28gZDIyBneU72EkBSA5hbAMb&count=1')
+.then(res=>res.json())
+.then(datos => {
+     fotos.src = datos[0].url
+     titulo.textContent = datos[0].title
+     
+     console.log(datos)
+})
